9ef2a63e31114646148bb3ec66191a7c05d01c53
[zwroesch@gmail.com] <zwroesch@gmail.com>
2023-02-08 13:03:41 -0800

    Designed homepage and added functions in app.js

--- app.js
+++ app.js
@@ -17,12 +17,22 @@ app.use(express.json());
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
-const dateTest = new Date(Date.now());
-const dateNow = dateTest.toISOString().slice(0, 10);
-console.log(dateNow);
+function toIsoString(date) {
+    var tzo = -date.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function (num) {
+            return (num < 10 ? '0' : '') + num;
+        };
 
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes());
+}
 
-
+const d = new Date();
+let current_date = toIsoString(d);
 
 
 // const schedule_response = await fetch('http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2022/By_Subject/CECS.html');
@@ -30,9 +40,9 @@ console.log(dateNow);
 // const address = "192.168.254.130";
 const port = 8000;
 app.listen(port);
-app.get('/', function(req, res) {
-    res.render('pages/index', {
+app.get('/', function (req, res) {
+    res.render('pages/home', {
         // EJS variable to server-side variable
-        dateNow: dateNow
+        current_date: current_date
     });
 })
