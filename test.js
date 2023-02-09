64a8c15ef583a2878da9b3d07bd92a18bdaae7ee
[zwroesch@gmail.com] <zwroesch@gmail.com>
2023-02-09 11:21:19 -0800

    Functions to turn time string to minutes and back

--- test.js
+++ test.js
@@ -1,18 +1,94 @@
-const time1 = "9-10:30AM";
-const time2 = "9:30-10AM";
-const time3 = "9:20-10:30PM";
-const time4 = "9-10AM";
+/**
+ * Time object for tuple of start and end time in minutes
+ */
+class Time {
+    #startMinutes;
+    #endMinutes;
 
-let hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-let minutes = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
-let arr = new Array(24);
-for (var i = 0; i < 24; i++){
-    arr[i] = new Array(minutes);
+    constructor(startTimeMinutes, endTimeMInutes) {
+        this.#startMinutes = startTimeMinutes;
+        this.#endMinutes = endTimeMInutes;
+    }
+
+    set start(start) {
+        this.#startMinutes = start;
+    }
+
+    get start() {
+        return this.#startMinutes;
+    }
+
+    set end(end) {
+        this.#endMinutes = end;
+    }
+
+    get end() {
+        return this.#endMinutes;
+    }
+
+    toTimeString(){
+        var returnValue;
+        if(this.#startMinutes >= 720){
+            
+        }
+
+    }
+
 }
 
 
-
-for (var i = 0; i < 24; i++){
-    console.log(arr[i]);
-    console.log(i);
+/**
+ * 
+ * @param {*} hours  String of hours
+ * @param {*} minutes  String of minutes
+ * @returns INTEGER the time of the day in minutes
+ */
+function toMinutes(hours, minutes) {
+    return ((Number(hours) * 60) + Number(minutes));
 }
+
+/**
+ * 
+ * @param {*} time String  of time 
+ * @returns parse the time string according to am or pm and return the time in minutes
+ */
+function parseTimeString(time) {
+    if (time.includes("AM")) {
+        var splitTimes = time.split("-");
+        var startTimeSplit = splitTimes[0].split(":");
+        var startTimeMinutes = toMinutes(startTimeSplit[0], startTimeSplit[1]);
+        var endTime = splitTimes[1].substring(0, splitTimes[1].indexOf("AM"));
+        var endTimeSplit = endTime.split(":");
+        var endTimeMinutes = toMinutes(endTimeSplit[0], endTimeSplit[1]);
+
+        const result = new Time(startTimeMinutes, endTimeMinutes);
+        return result;
+    }
+    else if (time.includes("PM")) {
+        var splitTimes = time.split("-");
+        var startTimeSplit = splitTimes[0].split(":");
+        var startTimeMinutes;
+        if(startTimeSplit[0] < 12){
+            startTimeMinutes = toMinutes(startTimeSplit[0], startTimeSplit[1]);
+        }
+        else{
+            startTimeMinutes = toMinutes((parseInt(startTimeSplit[0])), startTimeSplit[1]);
+        }
+        var endTime = splitTimes[1].substring(0, splitTimes[1].indexOf("PM"));
+        var endTimeSplit = endTime.split(":");
+
+        var endTimeMinutes = toMinutes((parseInt(endTimeSplit[0]) + 12), endTimeSplit[1]);
+
+        const result = new Time(startTimeMinutes, endTimeMinutes);
+        return result;
+    }
+    else {
+        console.log("Time not right format or N/A");
+    }
+    return 0;
+}
+var test = 750;
+var hour = Math.floor(750/60);
+var minutes = 750%60;
+console.log(hour);
+console.log(minutes);
